a75e1d629ec59b5acccc6642fa1ae216e4b39cf1
Nader Shbib <66152242+nadershbib@users.noreply.github.com>
2020-08-13 19:02:25 -0700

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import React,{useState} from "react";
+
+function App() {
+
+ const now = new Date().toLocaleTimeString();
+ const [time,setTime] = useState(now)
+ 
+ setInterval(change,1000);
+ 
+ function change(){
+const getTimeNow = new Date().toLocaleTimeString()
+   setTime(getTimeNow) 
+ }
+
+ return (
+   <div>
+     <h1>Time</h1>
+     <h1>{time}</h1>;
+          
+  </div>
+ ) 
+
+
+}
+export default App;
